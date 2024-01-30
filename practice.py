8e8196ef9aec7617a272136e3d5e7e7be2742916
Hank Caswell <david.h.caswell@proton.me>
2024-01-30 15:17:16 -0500

    did some more work on the function

--- practice.py
+++ practice.py
@@ -1,7 +1,16 @@
+def is_plural(bottles):
+    if bottles == 1:
+        print('bottle')
+    else:
+        print('bottles')
+       
+
+
+
 def ninety_nine(bottles):
     bottles = 99
     while bottles <= 1:
-        print(f'{bottles} bottles of beer on the wall {bottles} of beer')
+        print(f'{bottles} {is_plural(bottles)} of beer on the wall {bottles} of beer, take one down pass it around {bottles-1}{is_plural(bottles)} of beer on the wall')
         bottles -= 1
 
 ninety_nine(100)

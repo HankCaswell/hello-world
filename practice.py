ed22b1cf14521a44a0046fd60c7e3a62c3b15198
Hank Caswell <david.h.caswell@proton.me>
2024-01-30 15:11:54 -0500

    added a simple countdown function in python

---
+++ practice.py
@@ -0,0 +1,7 @@
+def ninety_nine(bottles):
+    bottles = 99
+    while bottles <= 1:
+        print(f'{bottles} bottles of beer on the wall {bottles} of beer')
+        bottles -= 1
+
+ninety_nine(100)
